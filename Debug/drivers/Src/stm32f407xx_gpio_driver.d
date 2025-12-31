df8b841984715d5c5eebc885dd72cf4cffe7f4e0
Vahid <vahidrspace@gmail.com>
2025-12-31 11:18:11 +0100

    GPIO driver APIs dev - clock control,init,deinit,read, write, toggle

---
+++ Debug/drivers/Src/stm32f407xx_gpio_driver.d
@@ -0,0 +1,6 @@
+drivers/Src/stm32f407xx_gpio_driver.o: \
+ ../drivers/Src/stm32f407xx_gpio_driver.c \
+ C:/Users/Vahid/Documents/Embedded_Courses/MCU1/my_workspace/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
+ C:/Users/Vahid/Documents/Embedded_Courses/MCU1/my_workspace/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+C:/Users/Vahid/Documents/Embedded_Courses/MCU1/my_workspace/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
+C:/Users/Vahid/Documents/Embedded_Courses/MCU1/my_workspace/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
