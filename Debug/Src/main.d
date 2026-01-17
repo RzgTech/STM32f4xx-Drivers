bd078e4eec793dfbb05ab88b3fe7e1d76bc56b12
Vahid <vahidrspace@gmail.com>
2026-01-17 21:14:39 +0100

    Apply .gitignore (stop tracking generated files)

--- Debug/Src/main.d
+++
@@ -1,3 +0,0 @@
-Src/main.o: ../Src/main.c \
- C:/Users/Vahid/Documents/Embedded_Courses/MCU1/my_workspace/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
-C:/Users/Vahid/Documents/Embedded_Courses/MCU1/my_workspace/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
