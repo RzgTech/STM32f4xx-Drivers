bfc38d207e44ba12710386aa28010e3c366d86dc
Vahid <vahidrspace@gmail.com>
2026-01-18 20:28:29 +0100

    SPI2_GPIOInits, SPI2_Inits

--- Src/005spi_tx_testing.c
+++ Src/005spi_tx_testing.c
@@ -4,6 +4,7 @@
  *  Created on: Jan 18, 2026
  *      Author: Vahid
  */
+#include<string.h>
 #include "stm32f407xx.h"
 
 /*
@@ -14,7 +15,7 @@
  * ALT Function mode: 5
  */
 
-void SPI_GPIOInits()
+void SPI2_GPIOInits()
 {
 	GPIO_Handle_t SPIPins;
 
@@ -34,20 +35,46 @@ void SPI_GPIOInits()
 	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;
 	GPIO_Init(&SPIPins);
 
+	//there is no slave in this exercise so we dont need MISO and NSS
 	//MISO
-	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
-	GPIO_Init(&SPIPins);
+	//SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+	//GPIO_Init(&SPIPins);
 
 	//NSS
-	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
-	GPIO_Init(&SPIPins);
+	//SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	//GPIO_Init(&SPIPins);
+
+}
+
+void SPI2_Inits(void)
+{
+	SPI_Handle_t SPI2handle;
+	SPI2handle.pSPIx = SPI2;
+	SPI2handle.SPIConfig.SPI_BusConfig = SPI_BUS_CONFIG_FD;
+	SPI2handle.SPIConfig.SPI_DeviceMode = SPI_DEVICE_MODE_MASTER;
+	SPI2handle.SPIConfig.SPI_SclkSpeed = SPI_SCLK_SPEED_DIV2; // max. possible speed : generates serial clock of 8 MHz
+	SPI2handle.SPIConfig.SPI_DFF = SPI_DFF_8BITS;
+	SPI2handle.SPIConfig.SPI_CPOL = SPI_CPOL_LOW;
+	SPI2handle.SPIConfig.SPI_CPHA = SPI_CPHA_LOW;
+	SPI2handle.SPIConfig.SPI_SSM = SPI_SSM_EN; //software slave management enabled for NSS pin - we dont need to use NSS for this exercise
+
+	SPI_Init(&SPI2handle);
+
+
 
 }
 
 int main(void)
 {
+	char user_data[] = "Hello World";
 	//this function is used to initialize the GPIO pins to behave as SPI2 pins
-	SPI_GPIOInits();
+	SPI2_GPIOInits();
+
+	SPI2_Inits();
+
+	SPI_SendData(SPI2, (uint8_t *)user_data, strlen(user_data));
+
+	while(1);
 
 	return 0;
 }
