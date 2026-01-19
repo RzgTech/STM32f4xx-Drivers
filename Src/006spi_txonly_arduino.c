6e09cf91314361ac33aef135aecfed00e608c68c
Vahid <vahidrspace@gmail.com>
2026-01-20 00:41:59 +0100

    adding SSOE function, SSM=0 (HW slave management)

--- Src/006spi_txonly_arduino.c
+++ Src/006spi_txonly_arduino.c
@@ -6,3 +6,93 @@
  */
 
 
+#include<string.h>
+#include "stm32f407xx.h"
+
+/*
+ * PB14 -> SPI2_MISO
+ * PB15 -> SPI2_MOSI
+ * PB13 -> SPI2_SCLK
+ * PB12 -> SPI2_NSS
+ * ALT Function mode: 5
+ */
+
+void SPI2_GPIOInits()
+{
+	GPIO_Handle_t SPIPins;
+
+	SPIPins.pGPIOx = GPIOB;
+
+	SPIPins.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+	SPIPins.GPIO_PinConfig.GPIO_PinAltFunMode = 5;
+	SPIPins.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP; // SPI works with pp, and open drain is not needed
+	SPIPins.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	SPIPins.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+
+	//SCLK
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+	GPIO_Init(&SPIPins);
+
+	//MOSI
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;
+	GPIO_Init(&SPIPins);
+
+	//there is no slave in this exercise so we dont need MISO and NSS
+	//MISO
+	//SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+	//GPIO_Init(&SPIPins);
+
+	//NSS
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GPIO_Init(&SPIPins);
+
+}
+
+void SPI2_Inits(void)
+{
+	SPI_Handle_t SPI2handle;
+	SPI2handle.pSPIx = SPI2;
+	SPI2handle.SPIConfig.SPI_BusConfig = SPI_BUS_CONFIG_FD;
+	SPI2handle.SPIConfig.SPI_DeviceMode = SPI_DEVICE_MODE_MASTER;
+	SPI2handle.SPIConfig.SPI_SclkSpeed = SPI_SCLK_SPEED_DIV8; // max. possible speed : generates serial clock of 8 MHz
+	SPI2handle.SPIConfig.SPI_DFF = SPI_DFF_8BITS;
+	SPI2handle.SPIConfig.SPI_CPOL = SPI_CPOL_LOW;
+	SPI2handle.SPIConfig.SPI_CPHA = SPI_CPHA_LOW;
+	SPI2handle.SPIConfig.SPI_SSM = SPI_SSM_DI; //hardware slave management enabled for NSS pin - we dont need to use NSS for this exercise
+
+	SPI_Init(&SPI2handle);
+
+
+
+}
+
+int main(void)
+{
+	char user_data[] = "Hello World";
+	//this function is used to initialize the GPIO pins to behave as SPI2 pins
+	SPI2_GPIOInits();
+
+	SPI2_Inits();
+
+	/*
+	* making SSOE 1 makes NSS output enable.
+	* The NSS pin is automatically managed by the hardware.
+	* i.e when SPE=1 , NSS will be pulled to low
+	* and NSS pin will be high when SPE=0
+	*/
+
+	SPI_SSOEConfig(SPI2, ENABLE);
+
+	//enable the SPI2 peripheral
+
+	SPI_PeripheralControl(SPI2, ENABLE);
+
+	SPI_SendData(SPI2, (uint8_t *)user_data, strlen(user_data));
+
+	//disable the SPI Peripheral
+	SPI_PeripheralControl(SPI2, DISABLE);
+
+	while(1);
+
+	return 0;
+}
