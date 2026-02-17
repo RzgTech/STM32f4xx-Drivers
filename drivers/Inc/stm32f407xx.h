799f0094f4367eda4e3b2f70812cbc969ec41c4f
Vahid <vahidrspace@gmail.com>
2026-02-17 01:07:51 +0100

    I2C driver API start

--- drivers/Inc/stm32f407xx.h
+++ drivers/Inc/stm32f407xx.h
@@ -151,6 +151,26 @@ typedef struct
 }SPI_RegDef_t;
 
 /*
+ * Peripheral register definition structure for I2C
+ */
+
+typedef struct
+{
+
+	__vo uint32_t CR1;
+	__vo uint32_t CR2;
+	__vo uint32_t OAR1;
+	__vo uint32_t OAR2;
+	__vo uint32_t DR;
+	__vo uint32_t SR1;
+	__vo uint32_t SR2;
+	__vo uint32_t CCR;
+	__vo uint32_t TRISE;
+	__vo uint32_t FLTR;
+
+}I2C_RegDef_t;
+
+/*
  * Peripheral register definition structure for RCC
  */
 
@@ -247,6 +267,9 @@ typedef struct
 #define SPI2						((SPI_RegDef_t *)SPI2_BASEADDR)
 #define SPI3						((SPI_RegDef_t *)SPI3_BASEADDR)
 
+#define I2C1						((I2C_RegDef_t *)I2C1_BASEADDR)
+#define I2C2						((I2C_RegDef_t *)I2C2_BASEADDR)
+#define I2C3						((I2C_RegDef_t *)I2C3_BASEADDR)
 
 /*
  * Clock Enable Macros for GPIOx peripherals
@@ -492,6 +515,69 @@ typedef struct
 #define SPI_SR_OVR					 	6
 #define SPI_SR_BSY					 	7
 #define SPI_SR_FRE					 	8
+
+
+/******************************************************************************************
+ *Bit position definitions of I2C peripheral
+ ******************************************************************************************/
+/*
+ * Bit position definitions I2C_CR1
+ */
+#define I2C_CR1_PE						0
+#define I2C_CR1_NOSTRETCH  				7
+#define I2C_CR1_START 					8
+#define I2C_CR1_STOP  				 	9
+#define I2C_CR1_ACK 				 	10
+#define I2C_CR1_SWRST  				 	15
+
+/*
+ * Bit position definitions I2C_CR2
+ */
+#define I2C_CR2_FREQ				 	0
+#define I2C_CR2_ITERREN				 	8
+#define I2C_CR2_ITEVTEN				 	9
+#define I2C_CR2_ITBUFEN 			    10
+
+/*
+ * Bit position definitions I2C_OAR1
+ */
+#define I2C_OAR1_ADD0    				 0
+#define I2C_OAR1_ADD71 				 	 1
+#define I2C_OAR1_ADD98  			 	 8
+#define I2C_OAR1_ADDMODE   			 	15
+
+/*
+ * Bit position definitions I2C_SR1
+ */
+
+#define I2C_SR1_SB 					 	0
+#define I2C_SR1_ADDR 				 	1
+#define I2C_SR1_BTF 					2
+#define I2C_SR1_ADD10 					3
+#define I2C_SR1_STOPF 					4
+#define I2C_SR1_RXNE 					6
+#define I2C_SR1_TXE 					7
+#define I2C_SR1_BERR 					8
+#define I2C_SR1_ARLO 					9
+#define I2C_SR1_AF 					 	10
+#define I2C_SR1_OVR 					11
+#define I2C_SR1_TIMEOUT 				14
+
+/*
+ * Bit position definitions I2C_SR2
+ */
+#define I2C_SR2_MSL						0
+#define I2C_SR2_BUSY 					1
+#define I2C_SR2_TRA 					2
+#define I2C_SR2_GENCALL 				4
+#define I2C_SR2_DUALF 					7
+
+/*
+ * Bit position definitions I2C_CCR
+ */
+#define I2C_CCR_CCR 					 0
+#define I2C_CCR_DUTY 					14
+#define I2C_CCR_FS  				 	15					0
 
 
 
---
+++ drivers/Src/stm32f407xx_i2c_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32f407xx_i2c_driver.c
+ *
+ *  Created on: 17 Feb 2026
+ *      Author: Vahid
+ */
+
+
