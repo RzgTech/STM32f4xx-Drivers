b1497a756115db9061d103bce04fd5d11500d3db
Vahid <vahidrspace@gmail.com>
2026-02-21 13:34:59 +0100

    I2C init API: computing the clock freq. of APB1 bus

--- drivers/Src/stm32f407xx_i2c_driver.c
+++ drivers/Src/stm32f407xx_i2c_driver.c
@@ -8,6 +8,9 @@
 #include "stm32f407xx.h"
 #include "stm32f407xx_i2c_driver.h"
 
+uint16_t AHB_PreScaler[8] = {2, 4, 8, 16, 64, 128, 256, 512};
+uint8_t APB1_PreScaler[4] = {2, 4, 8, 16};
+
 /*********************************************************************
  * @fn      		  - I2C_PeripheralControl
  *
@@ -56,30 +59,82 @@ void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi)
 	{
 		if(pI2Cx == I2C1)
 		{
-			I2C1_PCLK_EN();
+			I2C1EN_PCLK_EN();
 		} else if (pI2Cx == I2C2)
 		{
-			I2C2_PCLK_EN();
+			I2C1EN_PCLK_EN();
 		} else if (pI2Cx == I2C3)
 		{
-			I2C3_PCLK_EN();
+			I2C1EN_PCLK_EN();
 		}
 	}
 	else
 	{
 		if(pI2Cx == I2C1)
 		{
-			I2C1_PCLK_DI();
+			I2C1EN_PCLK_DI();
 		} else if (pI2Cx == I2C2)
 		{
-			I2C2_PCLK_DI();
+			I2C1EN_PCLK_DI();
 		} else if (pI2Cx == I2C3)
 		{
-			I2C3_PCLK_DI();
+			I2C1EN_PCLK_DI();
 		}
 	}
 }
 
+uint32_t RCC_GetPLLOutputClock(void)
+{
+	return 0;
+}
+
+uint32_t RCC_GetPCLK1Value(void)
+{
+	uint32_t pclk1, SystemClk;
+	uint8_t clksrc, temp, ahbp, apb1p;
+	clksrc = ((RCC->CFGR) >> 2) & 0x3; // shifting and masking
+
+	// for clock source
+	if (clksrc == 0)
+	{
+		SystemClk = 16000000;
+	}else if (clksrc == 1)
+	{
+		SystemClk = 8000000;
+	}else if (clksrc == 2)
+	{
+		SystemClk = RCC_GetPLLOutputClock();  //we'll not implement it in this course
+	}
+
+	//for ahb
+	temp = ((RCC->CFGR) >> 4) & 0xF;  //4 bits ->0xF
+
+	if (temp < 8)
+	{
+		ahbp = 1;
+	}else
+	{
+		ahbp = AHB_PreScaler[temp - 8];  //instead of writing different else if,
+										//we used the AHB_PreScaler to use only one
+	}									//else and decide about the prescaler value
+										//with the value of temp - 8
+
+	//for apb
+	temp = ((RCC->CFGR) >> 10) & 0x7;  //3 bits -> 0x7
+
+	if (temp < 4)
+	{
+		apb1p = 1;
+	}else
+	{
+		apb1p = APB1_PreScaler[temp - 4];
+	}
+
+	pclk1 = (SystemClk / ahbp) / apb1p;
+
+
+	return pclk1;
+}
 
 /*********************************************************************
  * @fn      		  - I2C_Init
@@ -97,6 +152,8 @@ void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi)
  */
 void I2C_Init(I2C_Handle_t *pI2CHandle)
 {
+	uint32_t tempreg = 0;
+	tempreg |= (pI2CHandle->I2C_Config.I2C_ACKControl << 10);
 
 }
 
