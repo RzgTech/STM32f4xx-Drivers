86d6da9b80bc4c31f86af085ff2aec74e0ca7620
Vahid <vahidrspace@gmail.com>
2026-03-15 19:51:29 +0100

    I2C adding interrupt related macros and APIs

--- drivers/Src/stm32f407xx_i2c_driver.c
+++ drivers/Src/stm32f407xx_i2c_driver.c
@@ -390,6 +390,17 @@ void I2C_MasterReceiveData(I2C_Handle_t *pI2C_Handle_t, uint8_t *pRxbuffer, uint
 	}
 }
 
+uint8_t I2C_MasterSendDataIT(I2C_Handle_t *pI2C_Handle_t, uint8_t *pTxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr)
+{
+
+}
+
+uint8_t I2C_MasterReceiveDataIT(I2C_Handle_t *pI2C_Handle_t, uint8_t *pRxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr)
+{
+
+}
+
+
 void I2C_ManageAcking(I2C_RegDef_t *pI2Cx, uint8_t EnorDi)
 {
 	if(EnorDi == I2C_ACK_ENABLE)
