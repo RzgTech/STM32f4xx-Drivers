1c9401e70b377f255a4512ce7273d10a7246fe8a
Vahid <vahidrspace@gmail.com>
2026-04-06 15:33:21 +0200

    creating rcc driver for separting some of the methods related to rcc

--- drivers/Src/stm32f407xx_i2c_driver.c
+++ drivers/Src/stm32f407xx_i2c_driver.c
@@ -7,9 +7,8 @@
 
 #include "stm32f407xx.h"
 #include "stm32f407xx_i2c_driver.h"
+#include "stm32f407xx_rcc_driver.h"
 
-uint16_t AHB_PreScaler[8] = {2, 4, 8, 16, 64, 128, 256, 512};
-uint8_t APB1_PreScaler[4] = {2, 4, 8, 16};
 static void I2C_GenerateStartCondition(I2C_RegDef_t *pI2Cx);
 static void I2C_ExecuteAddressPhaseWrite(I2C_RegDef_t *pI2Cx, uint8_t SlaveAddr);
 static void I2C_ExecuteAddressPhaseRead(I2C_RegDef_t *pI2Cx, uint8_t SlaveAddr);
@@ -175,58 +174,6 @@ void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi)
 	}
 }
 
-uint32_t RCC_GetPLLOutputClock(void)
-{
-	return 0;
-}
-
-uint32_t RCC_GetPCLK1Value(void)
-{
-	uint32_t pclk1, SystemClk;
-	uint8_t clksrc, temp, ahbp, apb1p;
-	clksrc = ((RCC->CFGR) >> 2) & 0x3; // shifting and masking
-
-	// for clock source
-	if (clksrc == 0)
-	{
-		SystemClk = 16000000;
-	}else if (clksrc == 1)
-	{
-		SystemClk = 8000000;
-	}else if (clksrc == 2)
-	{
-		SystemClk = RCC_GetPLLOutputClock();  //we'll not implement it in this course
-	}
-
-	//for ahb
-	temp = ((RCC->CFGR) >> 4) & 0xF;  //4 bits ->0xF
-
-	if (temp < 8)
-	{
-		ahbp = 1;
-	}else
-	{
-		ahbp = AHB_PreScaler[temp - 8];  //instead of writing different else if,
-										//we used the AHB_PreScaler to use only one
-	}									//else and decide about the prescaler value
-										//with the value of temp - 8
-
-	//for apb
-	temp = ((RCC->CFGR) >> 10) & 0x7;  //3 bits -> 0x7
-
-	if (temp < 4)
-	{
-		apb1p = 1;
-	}else
-	{
-		apb1p = APB1_PreScaler[temp - 4];
-	}
-
-	pclk1 = (SystemClk / ahbp) / apb1p;
-
-
-	return pclk1;
-}
 
 /*********************************************************************
  * @fn      		  - I2C_Init
